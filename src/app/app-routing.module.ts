f896fd98a632cb4a8981b361c602e34f41fa6a4d
khoile99 <khoi.le99@hcmut.edu.vn>
2023-08-06 16:42:33 +0700

    header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { HomePageComponent } from './pages/main-layout/home-page/home-page.component';
 
 const routes: Routes = [
   {
@@ -12,9 +14,14 @@ const routes: Routes = [
     path: 'register',
     component: RegisterComponent,
   },
+  {
+    path: 'home',
+    component: MainLayoutComponent,
+    children: [{ path: '', component: HomePageComponent }],
+  },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { HomePageComponent } from './pages/main-layout/home-page/home-page.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         LoginComponent,
-        RegisterComponent
+        RegisterComponent,
+        HeaderComponent,
+        MainLayoutComponent,
+        HomePageComponent
     ],
     imports: [
         BrowserModule,
